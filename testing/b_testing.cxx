83d5d2e03ac489bf13a7e6b86dc4abf2e4e922eb
mike <mike@pop-os.localdomain>
2020-07-07 13:16:33 +0200

    initial commit for master

---
+++ testing/b_testing.cxx
@@ -0,0 +1,206 @@
+/*
+ * a_test.cxx
+ * 
+ * Copyright 2020 mike <mike@pop-os>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+/*
+ * Consider a matrix(4,5) F(W,H) F(3,4)
+ */
+
+#include <iostream>
+#include <array>
+#include <vector>
+#include <utility>
+
+const int W = 1;
+const int H = 1;
+
+//const int X = W+1;
+//const int Y = H+1;
+
+enum Status {avail, used, parent};
+
+typedef struct Node Node;
+
+typedef struct {
+	int to_x, to_y;	// bounds checking
+	Node *next;		// pointer to next node
+	Status status;
+} Step;
+
+enum NodeStat { root, path_node, home };
+
+typedef struct Node {
+	std::vector<Step> steps;
+	NodeStat nodestat;
+} Node;
+
+typedef std::array< std::array<Node,W+1>, H+1> Lattice;
+
+//-------------------------------------------------
+void initalise_lattice(Lattice &l);
+
+/* Steps are defined as follows:
+ * N 1,2,3    E 1,2,3    S 1,2,3    E 1,2,3
+ * NE 5,8     SE 5,8     SW 5,8     NE 5,8
+ * 0 <= to_x <= W    0 <= to_y <= H
+ */
+ 
+void initalise_lattice(Lattice &l){
+	Step s;
+	for(auto x = 0; x <= W; ++x) {
+		for(auto y = 0; y <= H; ++y) {
+			Node &n = l[x][y];
+			// initialise the vector of Step for this node
+			// x & y are node co-ords
+			n.steps.clear();
+			// consider 4 * 1,2,3 orthogonal steps
+			for(int ds = 1; ds <= 3; ++ds) {				
+				s.to_x = x;	s.to_y = y+ds;
+				if(s.to_y <= H) {
+					s.next = &(l[x][y+ds]);		
+					s.status = avail;
+					n.nodestat = path_node;
+					n.steps.push_back(s); // N 1
+				}
+					
+				s.to_x = x+ds;	s.to_y = y;
+				if(s.to_x <= W) {
+					s.next = &(l[x+ds][y]);		
+					s.status = avail; 
+					n.nodestat = path_node;
+					n.steps.push_back(s); // E 1
+				}
+											
+				s.to_x = x;	s.to_y = y-ds;
+				if(s.to_y >= 0) {
+					s.next = &(l[x][y-ds]);		
+					s.status = avail; 
+					n.nodestat = path_node;
+					n.steps.push_back(s); // S 1
+				}
+				
+				s.to_x = x-ds;	s.to_y = y;
+				if(s.to_x >= 0) {
+					s.next = &(l[x-ds][y]);		
+					s.status = avail; 
+					n.nodestat = path_node;
+					n.steps.push_back(s); // W 1
+				}
+			} // for ds...
+			
+			// now consider possible diagonal steps
+			int dx = 4; int dy = 3;
+			
+			s.to_x = x+dx;	s.to_y = y+dy;
+			if((s.to_x <= W)and(s.to_y <= H)) {
+				s.next = &(l[x+dx][y+dy]);		
+				s.status = avail;
+				n.nodestat = path_node;
+				n.steps.push_back(s); // NE QUADRANT 5
+			}
+			s.to_x = x+dy;	s.to_y = y+dx;	// note meaning of dx & dx exchanged
+			if((s.to_x <= W)and(s.to_y <= H)) {
+				s.next = &(l[x+dx][y+dy]);		
+				s.status = avail;
+				n.nodestat = path_node;
+				n.steps.push_back(s); // NE QUADRANT 5
+			}
+			
+			s.to_x = x+dx;	s.to_y = y-dy;
+			if((s.to_x <= W)and(s.to_y >= 0)) {
+				s.next = &(l[x+dx][y-dy]);		
+				s.status = avail;
+				n.nodestat = path_node;
+				n.steps.push_back(s); // SE QUADRANT 5
+			}
+			s.to_x = x+dy;	s.to_y = y-dx;	// note meaning of dx & dx exchanged
+			if((s.to_x <= W)and(s.to_y >= 0)) {
+				s.next = &(l[x+dy][y+dx]);		
+				s.status = avail;
+				n.nodestat = path_node;
+				n.steps.push_back(s); // SE QUADRANT 5
+			}
+			
+			s.to_x = x-dx;	s.to_y = y-dy;
+			if((0 <= s.to_x)and(0 <= s.to_y)) {
+				s.next = &(l[x-dx][y-dy]);		
+				s.status = avail;
+				n.nodestat = path_node;
+				n.steps.push_back(s); // SW QUADRANT 5
+			}
+			s.to_x = x-dy;	s.to_y = y-dx;	// note meaning of dx & dx exchanged
+			if((0 <= s.to_x)and(0 <= s.to_y)) {
+				s.next = &(l[x-dy][y-dx]);		
+				s.status = avail;
+				n.nodestat = path_node;
+				n.steps.push_back(s); // SW QUADRANT 5
+			}
+			
+			s.to_x = x-dx;	s.to_y = y+dy;
+			if((0 <= s.to_x)and(s.to_y <= H)) {
+				s.next = &(l[x-dx][y+dy]);		
+				s.status = avail;
+				n.nodestat = path_node;
+				n.steps.push_back(s); // NE QUADRANT 5
+			}
+			s.to_x = x-dy;	s.to_y = y+dx;	// note meaning of dx & dx exchanged
+			if((0 <= s.to_x)and(s.to_y <= H)) {
+				s.next = &(l[x-dy][y-dx]);		
+				s.status = avail;
+				n.nodestat = path_node;
+				n.steps.push_back(s); // NE QUADRANT 5
+			}						
+		} // for y...
+	} // for x...
+	// adjust the node status for root & home
+	l[0][0].nodestat = root;
+	l[W][H].nodestat = home;	
+}
+//-------------------------------------------------
+
+bool explore(Node *n, int &count);
+
+bool explore(Node *n, int &count) {
+	if(n->nodestat == home) {
+		count += 1;
+		return true;
+	}
+	for(auto c = n->steps.begin(); c != n->steps.end(); ++c) {
+		if ((*c).status == used) continue;
+		if(explore((*c).next, count)) return true;
+	}
+	for(auto c = n->steps.begin(); c != n->steps.end(); ++c) (*c).status = avail;
+	return false;
+}
+//----------------------------------------------------------------------
+
+int main(int argc, char **argv)
+{
+	Lattice l;
+	
+	//Initialise all Lattice values
+	initalise_lattice(l);
+	int path_count = 0;
+	bool result = explore(&(l[0][0]), path_count);
+	std::cout << "Explore root returned. Path count = " << path_count << std::endl;
+}
+
